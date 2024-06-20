de0111b668f4a19939ec13806d0e78ff9c610eca
stark <akshaystark3000@gmail.com>
2024-06-21 01:55:03 +0530

    clerk and webhook done

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://ready-oriole-2.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  export default authConfig;
